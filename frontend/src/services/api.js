4d53271429d3838180ec82f14bab42f4bda487ed
Mohammad Hashem <mohamedhashem313@outlook.com>
2025-08-20 13:16:58 +0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,20 +1,28 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://192.168.10.209:5000/api', // Your backend URL
-    headers: {
-        'Content-Type': 'application/json',
-    },
+    baseURL: '/api'
+});
+
+apiClient.interceptors.request.use(config => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
 });
 
 apiClient.interceptors.response.use(
     response => response,
     error => {
         if (error.response && error.response.status === 401) {
-            localStorage.removeItem('authToken');
-            localStorage.removeItem('user');
-            // Reloading the page will force the user to the login screen
-            window.location.href = '/login'; 
+            if (window.location.pathname !== '/login') {
+                localStorage.removeItem('authToken');
+                localStorage.removeItem('user');
+                window.location.href = '/login'; 
+            }
         }
         return Promise.reject(error);
     }
