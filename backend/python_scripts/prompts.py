d45923841bd9da1bc76ec44a6fae8690b7a12e37
Mohammad Hashem <mohamedhashem313@outlook.com>
2025-08-27 14:30:04 +0300

    Update prompts.py

--- backend/python_scripts/prompts.py
+++ backend/python_scripts/prompts.py
@@ -3,11 +3,12 @@ prompt_2 = f"""
     Extract and return the following fields:
 
     - transaction_id: The ID looks like this: "E18189547202502171718GVpGtoyM2R3". It can include digits and uppercase/lowercase letters. However, be VERY careful: Return the ID **exactly as shown**. If not found, fallback to transaction_number.
-    - amount: The total transaction amount (with no currency symbol). Example "6.790,00", display it as "6,790.00" in the JSON output. "357,00" should be displayed as "357.00". "357" should be displayed as "357.00". "2.000" should be displayed as "2,000.00". "2000" should be displayed as "2,000.00".
+    - amount: The total transaction amount (with no currency symbol). Example "6.790,00", display it as "6,790.00" in the JSON output. "357,00" should be displayed as "357.00". "357" should be displayed as "357.00". "2.000" should be displayed as "2,000.00". "2000" should be displayed as "2,000.00". 10.000,00 should be displayed as "10,000.00". 10000,00 should be displayed as "10,000.00". 10000 should be displayed as "10,000.00". 1000 should be displayed as "1,000.00". 1.000 should be displayed as "1,000.00". 1.000,50 should be displayed as "1,000.50".
     - sender: Information about the sender (issuer) of the payment.
         - name: The name of the sender (business or individual).
     - recipient: Information about the recipient (receiver) of the payment.
         - name: The name of the recipient (business or individual).
+    sometimes th sender and reciever can be swapped, if you find "Trkbit" or "Trkbit Tecnologia E Informacao Ltda" or "TRKBIT TECNOLOGIA E INFORMACAO LTDA	", as sender name, regardless of case sensitivity, it is the recipient.
     - additional_data: Any extra data related to the transaction.
     - image_type: Classify the image into one of the following types based on its visual context:
         - replay: A photo of another screen (like a phone or tablet) displaying a receipt only. Look for indicators such as phone, screen borders, hands holding a device, or double screen brightness.
