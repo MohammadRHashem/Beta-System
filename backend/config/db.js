cfeed7265f7af5e7d6efa63a132639ffee33dfb2
Mohammad Hashem <mohamedhashem313@outlook.com>
2025-09-09 09:13:04 +0300

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0,
     dateStrings: true,
-    timezone: '-03:00'
+    timezone: '+00:00'
 });
 
 console.log('[DB] Database connection pool created.');
