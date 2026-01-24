0a4c13e1e9c5d71462a6247b8609434530d8a304
Mohammad Hashem <mohamedhashem313@outlook.com>
2026-01-24 11:45:11 -0300

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import { FaWhatsapp, FaBullhorn, FaRobot, FaCog, FaKey, FaKeyboard, FaFileInvoiceDollar, FaChartLine, FaCheckCircle, FaRoute, FaUniversity, FaUsers, FaBitcoin, FaCalendarAlt, FaUserFriends, FaCheckDouble, FaClipboardList, FaCodeBranch } from "react-icons/fa";
+import { FaWhatsapp, FaBullhorn, FaRobot, FaCog, FaKey, FaKeyboard, FaFileInvoiceDollar, FaChartLine, FaCheckCircle, FaRoute, FaUniversity, FaUsers, FaBitcoin, FaCalendarAlt, FaUserFriends, FaCheckDouble, FaClipboardList, FaCodeBranch, FaShieldAlt } from "react-icons/fa";
 import { usePermissions } from '../context/PermissionContext';
 
 const SidebarContainer = styled.nav`
